3641b61b53c0e2cabc31792b1a21f94516bdead0
Andrew Champion <awchampion@gmail.com>
2018-08-20 14:04:52 -0700

    license-compatible isclose()

--- roughly/shim.py
+++ roughly/shim.py
@@ -2,7 +2,17 @@
 
 
 # pylint: disable=invalid-name
-def isclose(a, b, rel_tol, abs_tol):
+def compare_pct(a, b, rel_tol):
+    # type: (float, float, float) -> float
+    """Calculate max difference as percentage of larger number"""
+    max_val = sorted((abs(a), abs(b))).pop()  # type: float
+
+    return max_val * rel_tol
+
+
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
     # type: (float, float, float, float) -> bool
     """Implement math.isclose where not builtin"""
-    return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+    diff = abs(a - b)
+
+    return diff <= abs_tol and diff < compare_pct(a, b, rel_tol)
